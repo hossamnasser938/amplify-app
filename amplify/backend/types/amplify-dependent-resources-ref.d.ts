a1de4f52dfb04d11f739a5fdd4bb10364eeb43b7
hossamnasser938 <hossamnasser938@gmail.com>
2023-09-29 16:01:38 +0300

    Integrate AppSync (GraphQL APIS) & S3 (Storage)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyapp1e48006f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3amplifyapp": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
